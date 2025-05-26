cf83bfc7d4193082fe6e35a3ef4427694fa441c2
전혜미@아이브랩 <hmjeon@aiblab.co.kr>
2025-05-26 15:55:18 +0900

    firbase auth 세팅

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,18 @@
+import { useState } from 'react';
+import { getAuth } from 'firebase/auth';
+
+import { app } from 'firebaseApp';
 import Layout from 'components/Layout';
 import Router from 'components/Router';
 
 function App() {
+  const auth = getAuth(app);
+
+  const [isAuthenticated, setIsAuthenticated] = useState<boolean>(
+    !!auth?.currentUser
+  );
+  console.log(isAuthenticated);
+
   return (
     <Layout>
       <Router />
